df3e627f304ae6b04aaeaa8d09cc35a87364c3db
Alan Sabu~ <alansabu6046@gmail.com>
2026-06-11 02:17:09 +0530

    feat:hashing using bcrypt complete

--- server/src/features/auth/shared/passwordHash.utils.ts
+++ server/src/features/auth/shared/passwordHash.utils.ts
@@ -1,2 +1,18 @@
 import { hash, compare } from 'bcrypt-ts';
 
+const SALT_ROUNDS = 10;
+
+//HASING PART
+
+export const hashPassword = async(password:string):Promise<string> =>{
+
+    return hash(password, SALT_ROUNDS);
+}
+
+//verying password
+
+export const verifyPassword = async(plain:string,hashed:string):Promise<boolean> =>{
+
+    return compare(plain, hashed)
+
+}
