2bfede2e2be50154b76ef76c3ef1fd8b50aad59a
Alan Sabu~ <alansabu6046@gmail.com>
2026-06-15 02:24:51 +0530

    feat :service layer logic is finished

--- server/src/features/auth/loginwithpass/login.services.ts
+++ server/src/features/auth/loginwithpass/login.services.ts
@@ -2,6 +2,9 @@ import { loginCredentials, loginPayload } from "../shared/auth.types.js";
 import { findbyemail } from "../shared/auth.repository.js"; 
 import { verifyPassword } from "../shared/passwordHash.utils.js";
 import { createSession } from "../shared/session.repository.js";
+import { redisClient } from "../../../config/connectredis.js";
+
+//login with pass fn
 export const loginwithpass = async(credentials:loginCredentials):Promise<loginPayload> =>{
 
  
@@ -11,21 +14,52 @@ export const loginwithpass = async(credentials:loginCredentials):Promise<loginPa
     
     const hashPassword = user.password;
     
+
+    // vefifying client cred
     const isMatch = verifyPassword(credentials.password,hashPassword)
     
     if(!isMatch){
         throw new Error("Invalid username or password");
     }
 
+    // sessionid creation in pg
   const sessionId = await createSession({
     userId: user.userId,
     expiredAt: new Date(Date.now() + 24 * 60 * 60 * 1000) 
   });
 
-  return {
+
+  // login payload after auth
+  const loginpayload ={
+
+
     sessionId,
     userId: user.userId,
     username: user.username,
     status: user.status
+  
   }
-    }   
+
+
+
+  //serialising payload
+
+  const serialisedPayload = JSON.stringify(loginpayload)
+
+
+//passing serialised payload to redis 
+
+const redisKey = `session:${sessionId}`
+
+  await redisClient.set(redisKey,serialisedPayload,{
+
+    EX:24*60*60
+  })
+
+  return loginpayload;
+
+}
+
+
+    
+    
